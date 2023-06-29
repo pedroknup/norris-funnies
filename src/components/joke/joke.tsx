9603259c3f6897bdf3dead3c5b5b3a8f8feb59e7
Pedro Knup <pedro.knup@mediamonks.com>
2023-06-29 18:00:44 +0200

    test: add test for Joke component

--- src/components/joke/joke.tsx
+++ src/components/joke/joke.tsx
@@ -7,7 +7,7 @@ interface JokeProps {
 
 function Joke({ joke }: JokeProps): React.ReactNode {
     return (
-        <div className="joke">
+        <div className="joke" data-testid="joke">
             <img src={joke.iconUrl} alt="Chuck Norris" />
             <span>{joke.value}</span>
         </div>

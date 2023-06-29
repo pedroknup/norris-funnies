e0ea5725cbd480df1bc0cef81c7866fa79c08725
Pedro Knup <pedro.knup@mediamonks.com>
2023-06-29 16:59:52 +0200

    feat: add empty home-page component
    Remove react-create-app placeholder code

---
+++ src/pages/home-page/home-page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './home-page.css';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      
+    </div>
+  );
+}
+
+export default HomePage;

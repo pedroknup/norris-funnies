2f6a6144f16fdeb0da17ab53f1f1cde1d5cbb7e2
Pedro Knup <pedro.knup@mediamonks.com>
2023-06-30 13:32:08 +0200

    test: implement svgTransform

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ const { compilerOptions } = require('./tsconfig')
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.svg$': '<rootDir>/svgTransform.js',
+    },
     moduleNameMapper: {
         ...pathsToModuleNameMapper(compilerOptions.paths, {
             prefix: '<rootDir>/src/',

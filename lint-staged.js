f79ea6ba881e8d4d5dae7186cd8e9e8bbfa7b4e7
Pedro Knup <pedro.knup@mediamonks.com>
2023-06-30 23:09:08 +0200

    chore: bring test back to lint-staged

--- lint-staged.js
+++ lint-staged.js
@@ -1,7 +1,7 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint --max-warnings=0',
-        // 'craco test -- --config=jest.config.js --watchAll=false --findRelatedTests --passWithNoTests',
+        'craco test --verbose -- --config=jest.config.js --watchAll=false',
         () => 'tsc-files --noEmit',
     ],
     '*.{js,jsx,ts,tsx,json,css}': ['prettier --write'],

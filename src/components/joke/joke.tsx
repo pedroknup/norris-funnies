9fe384cf8871bc2f5bd47670be4fa8200d7a52da
Pedro Knup <pedro.knup@mediamonks.com>
2023-06-30 09:45:27 +0200

    feat: add like button to Joke and improve its UI

--- src/components/joke/joke.tsx
+++ src/components/joke/joke.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './joke.scss'
+import { ReactComponent as OutlineHeartIcon } from '@assets/icons/heart.svg'
 import { type ChuckNorrisJoke } from '@models/chuck-norris-joke'
 
 interface JokeProps {
     joke: ChuckNorrisJoke
+    onJokeLikeClick?: () => void
 }
 
-function Joke({ joke }: JokeProps): React.ReactNode {
+function Joke({ joke, onJokeLikeClick }: JokeProps): React.ReactNode {
     return (
-        <div className="joke" data-testid="joke">
+        <div className={`joke ${joke.liked ? 'liked' : ''}`} data-testid="joke">
             <span>{joke.value}</span>
+            <button onClick={onJokeLikeClick} className="heart-button">
+                <OutlineHeartIcon />
+            </button>
         </div>
     )
 }

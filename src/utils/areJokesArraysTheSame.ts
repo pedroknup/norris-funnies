c12992c79f631696ebc1a53f6ac4cb631666b968
Pedro Knup <pedro.knup@mediamonks.com>
2023-06-30 23:32:35 +0200

    chore: delete unused util

--- src/utils/areJokesArraysTheSame.ts
+++
@@ -1,8 +0,0 @@
-import { type ChuckNorrisJoke } from '@models/chuck-norris-joke'
-
-export default function areJokesArraysTheSame(
-    arr1: ChuckNorrisJoke[],
-    arr2: ChuckNorrisJoke[],
-): boolean {
-    return arr1.every(obj => arr2.some(obj2 => obj2.id === obj.id))
-}

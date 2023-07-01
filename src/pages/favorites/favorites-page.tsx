c4243587c09a1f0361a09f914d6d92fd21cf7543
Pedro Knup <pedro.knup@mediamonks.com>
2023-07-01 13:36:42 +0200

    feat: add favorites page

---
+++ src/pages/favorites/favorites-page.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import './favorites-page.scss'
+import Joke from '@components/joke'
+import { AnimatedList } from 'react-animated-list'
+import { type ChuckNorrisJoke } from '@models/chuck-norris-joke'
+
+const BACKGROUND_URL = `${process.env.PUBLIC_URL}/assets/chuck-norris-background.png`
+
+function FavoritesPage(): React.ReactNode {
+    const [jokes, setJokes] = React.useState<ChuckNorrisJoke[]>([])
+
+    useEffect(() => {
+        const jokes = JSON.parse(localStorage.getItem('likedJokes') ?? '[]')
+        setJokes(jokes)
+    }, [])
+
+    const handleJokeLikeToggle = (id: string): void => {
+        const updatedJokes = jokes.filter(joke => joke.id !== id)
+        setJokes(updatedJokes)
+        localStorage.setItem('likedJokes', JSON.stringify(updatedJokes))
+    }
+
+    return (
+        <div className="favorites-page">
+            <div className="background-wrapper">
+                <img src={BACKGROUND_URL} />
+            </div>
+            <div className="title-wrapper">
+                <h1 className="title">Liked Jokes</h1>
+            </div>
+
+            <div className="jokes-container" data-testid="jokes-container">
+                <AnimatedList animation={'grow'}>
+                    {jokes.map(joke => (
+                        <Joke
+                            onLikeToggle={() => {
+                                handleJokeLikeToggle(joke.id)
+                            }}
+                            key={joke.id}
+                            joke={joke}
+                            data-testid="joke"
+                        />
+                    ))}
+                </AnimatedList>
+            </div>
+        </div>
+    )
+}
+
+export default FavoritesPage
